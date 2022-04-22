570dd68168a3a0f4b99036396450f7c38bf3261e
ten-redpanda <kuwahata@logly.co.jp>
2022-04-22 14:23:49 +0900

    レイアウトとルート設定

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get '/help', to: 'static_pages#help'
-  get  '/about', to: 'static_pages#about'
-  get  '/contact', to: 'static_pages#contact'
+  get "/signup", to: "users#new"
+  get "/help", to: "static_pages#help"
+  get "/about", to: "static_pages#about"
+  get "/contact", to: "static_pages#contact"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
