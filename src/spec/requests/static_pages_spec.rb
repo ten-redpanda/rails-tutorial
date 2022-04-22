021a0949bf4d244b3a919045e077c91d0e58b3e7
ten-redpanda <kuwahata@logly.co.jp>
2022-04-22 14:28:15 +0900

    static_pagesテストのgetするルートを変更

--- src/spec/requests/static_pages_spec.rb
+++ src/spec/requests/static_pages_spec.rb
@@ -1,30 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe "StaticPages", type: :request do
-  describe "GET /home" do
+  describe "GET root" do
     it "renders a successful response" do
-      get static_pages_home_url
+      get root_path
       expect(response).to be_successful
     end
   end
 
   describe "GET /help" do
     it "renders a successful response" do
-      get static_pages_help_url
+      get help_path
       expect(response).to be_successful
     end
   end
 
   describe "GET /about" do
     it "renders a successful response" do
-      get static_pages_about_url
+      get about_path
       expect(response).to be_successful
     end
   end
 
   describe "GET /contact" do
     it "renders a successful response" do
-      get static_pages_contact_url
+      get contact_path
       expect(response).to be_successful
     end
   end
